b7ba63a3bd8dcbed096d95f122006cb335eb0a0f
Pranav Sharma <90517976+Pranav-Sharma1@users.noreply.github.com>
2022-12-15 19:04:14 +0000

    Add files via upload

---
+++ page/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default function Home() {
+  return (
+    <>
+    <Head> <meta name="viewport" content="width=device-width, initial scale=1" /></Head>
+    <h1>Hello World</h1>
+    </>
+  )
+}
